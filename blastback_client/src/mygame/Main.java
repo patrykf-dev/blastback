a5f1f7fc4953f3a2af4c22e63f16942e34557eef
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2018-12-15 00:23:53 +0100

    Fixed Player collisions

--- blastback_client/src/mygame/Main.java
+++ blastback_client/src/mygame/Main.java
@@ -2,7 +2,9 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.KeyInput;
@@ -11,12 +13,13 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -28,9 +31,9 @@ public class Main extends SimpleApplication
     BulletAppState bulletAppState;
     
     //Player fields
-    RigidBodyControl player_rb;
+    CharacterControl player_rb;
     boolean left = false, right = false, up = false, down = false;
-    float moveSpeed = 10f;
+    float moveSpeed = 0.1f;
     Vector3f moveDirection = new Vector3f();
     
     public static void main(String[] args) {
@@ -44,13 +47,12 @@ public class Main extends SimpleApplication
         flyCam.setEnabled(false);
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
-        flyCam.setMoveSpeed(20f);
-        cam.setLocation(new Vector3f(0f, 30f, -0.1f));
+        cam.setLocation(new Vector3f(0f, 20f, 0.1f));
         cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
         
-        initKeys();
         initScene();
         initPlayer();
+        initKeys();
     }
     
     private void initScene()
@@ -59,7 +61,6 @@ public class Main extends SimpleApplication
         CollisionShape col = CollisionShapeFactory.createMeshShape(scene);
         RigidBodyControl map_rb = new RigidBodyControl(0.0f);
         scene.addControl(map_rb);
-        map_rb.setFriction(0f);
         
         rootNode.attachChild(scene);
         bulletAppState.getPhysicsSpace().add(map_rb);
@@ -86,12 +87,17 @@ public class Main extends SimpleApplication
         player_mat.setColor("Color", ColorRGBA.Blue);
         player.setMaterial(player_mat);
 
-        player_rb = new RigidBodyControl(1f);
+        CollisionShape shape = new BoxCollisionShape(new Vector3f(0.5f, 1f, 0.5f));
+        CharacterControl player_cc = new CharacterControl(shape, 0.05f);
+        player_cc.setGravity(new Vector3f(0f, -1f, 0f));
+        player_cc.setJumpSpeed(20f);
+        player_cc.setFallSpeed(30f);
+        player_rb = player_cc;
         player.addControl(player_rb);
-        player.setLocalTranslation(0f, 2.2f, 0f);
         rootNode.attachChild(player);
-        player_rb.setKinematic(false);
         bulletAppState.getPhysicsSpace().add(player_rb);
+        player_rb.setPhysicsLocation(new Vector3f(0f, 2.2f, 0f));
+        player_rb.setViewDirection(new Vector3f(1f, 0f, 0f));
     }
     
     private void playerUpdate(float tpf)
@@ -114,14 +120,20 @@ public class Main extends SimpleApplication
         {
             moveDirection.addLocal(0f, 0f, 1f);
         }
-        moveDirection.normalizeLocal().mult(moveSpeed);
-        player_rb.setPhysicsRotation(Quaternion.IDENTITY);
-        player_rb.setLinearVelocity(moveDirection);
+        moveDirection.normalizeLocal().multLocal(moveSpeed);
+        player_rb.setWalkDirection(moveDirection);
+        //Logger.getLogger(Main.class.getName()).log(Level.INFO, moveDirection.toString());
+    }
+    
+    private void cameraUpdate(float tpf)
+    {
+        cam.setLocation(player_rb.getPhysicsLocation().add(0f, 20f, 0f));
     }
     
     @Override
     public void simpleUpdate(float tpf) {
         playerUpdate(tpf);
+        cameraUpdate(tpf);
     }
 
     @Override
