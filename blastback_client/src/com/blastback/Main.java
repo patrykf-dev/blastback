73ab7787dc58a1d20637708e076ca0f0fa71ee3d
Patryk <patryk.gsrmeen@gmail.com>
2018-12-15 15:01:59 +0100

    Organize imports
    Orgaznize imports in order to succesfully release game. Otherwise "error: package com.sun.istack.internal.logging does not exist"

--- blastback_client/src/com/blastback/Main.java
+++ blastback_client/src/com/blastback/Main.java
@@ -10,18 +10,10 @@ import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import java.util.HashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
