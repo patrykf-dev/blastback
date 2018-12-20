b13db301a0629ed67a64c6bd4bff8d61a852e74e
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2018-12-20 16:59:42 +0100

    Add client-side simulation logic
    No listeners yet implemented.

--- blastback_client/src/com/blastback/appstates/SimulationAppState.java
+++ blastback_client/src/com/blastback/appstates/SimulationAppState.java
@@ -5,25 +5,30 @@ import com.blastback.controls.CharacterManagerControl;
 import com.blastback.shared.messages.data.PlayerStateInfo;
 import com.jme3.app.Application;
 import com.jme3.app.state.BaseAppState;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import java.util.List;
 
 public class SimulationAppState extends BaseAppState
 {
     private GameClient _app;
-    private int _clientId;
-    private List<PlayerStateInfo> _clientsInfo;
+    private BulletAppState _bulletAppState;
+    private NetworkAppState _network;
     
     private List<CharacterManagerControl> _characters;
-    
-    public void setClientsInfo(String msg)
-    {
-        
-    }
     
     @Override
     protected void initialize(Application app)
     {
         _app = (GameClient)app;
+        _bulletAppState = _app.getStateManager().getState(BulletAppState.class);
+        _network = _app.getStateManager().getState(NetworkAppState.class);
     }
 
     @Override
@@ -35,14 +40,167 @@ public class SimulationAppState extends BaseAppState
     @Override
     protected void onEnable()
     {
-        
+        Node root = _app.getRootNode();
+        PhysicsSpace space = _bulletAppState.getPhysicsSpace();
+        for(CharacterManagerControl character : _characters)
+        {
+            character.enableCharacter(root, space);
+        }
     }
     
 
     @Override
     protected void onDisable()
     {
+        Node root = _app.getRootNode();
+        PhysicsSpace space = _bulletAppState.getPhysicsSpace();
+        for (CharacterManagerControl character : _characters)
+        {
+            character.disableCharacter(root, space);
+        }
+    }
+    
+    
+    public void setClientsInfo(List<PlayerStateInfo> playerStates)
+    {    
+        removeLocalPlayerState(playerStates);
         
+        if (playerStates.size() != _characters.size())
+        {
+            correctCharacters(playerStates);
+        }
+        
+        int processed = updateCharacters(playerStates);
+        
+        if(processed != playerStates.size())
+        {
+            correctCharacters(playerStates);
+        }
+    }
+    
+    /**
+     * Method updates number of simulated characters based on received list of player states. 
+     * Then all characters are reset (id, position and rotation) to match given state.
+     * @param playerStates List of characters' states that need to be simulated (must not contain local player state!).
+     */
+    private void correctCharacters(List<PlayerStateInfo> playerStates)
+    {
+        disableCharacters();
+        
+        setCharacterListSize(playerStates.size());
+        
+        for(int i = 0; i < playerStates.size(); i++)
+        {
+            _characters.get(i).setFromState(playerStates.get(i));
+        }
+        
+        enableCharacters();
+    }
+    
+    /**
+     * Creates a spatial with necessary controls and returns its CharacterManagerControl.
+     * @return 
+     */
+    private CharacterManagerControl createCharacter()
+    {
+        Spatial character = _app.getAssetManager().loadModel("Models/Player.j3o");
+
+        CollisionShape shape = new CapsuleCollisionShape(0.5f, 1f, 1);
+        CharacterControl charControl = new CharacterControl(shape, 0.1f);
+        charControl.setGravity(new Vector3f(0f, -1f, 0f));
+
+        // Add controls to spatials
+        character.addControl(charControl);
+
+        CharacterManagerControl manager = new CharacterManagerControl();
+        character.addControl(manager);
+
+        return manager;
+    }
+    
+    /**
+     * Removes a state that has an id equal to this client id.
+     * @param playerStates Received list of player states from the server.
+     */
+    private void removeLocalPlayerState(List<PlayerStateInfo> playerStates)
+    {
+        int localId = _network.getClientInstance().getId();
+        for (int i = 0; i < playerStates.size(); i++)
+        {
+            if (playerStates.get(i).getClientId() == localId)
+            {
+                playerStates.remove(i);
+                break;
+            }
+        }
+    }
+    
+    /**
+     * Method updates local character spatials based on received list of player states.
+     * @param playerStates
+     * @return 
+     */
+    private int updateCharacters(List<PlayerStateInfo> playerStates)
+    {
+        int processed = 0;
+        for (PlayerStateInfo state : playerStates)
+        {
+            for (CharacterManagerControl character : _characters)
+            {
+                if (character.getId() == state.getClientId())
+                {
+                    character.setPosition(state.getPlayerState().getLocalTranslation());
+                    character.setRotation(state.getPlayerState().getLocalRotation());
+                    processed++;
+                }
+            }
+        }
+        return processed;
+    }
+    
+    /**
+     * Disables all characters currently simulated (detaches from node and physics space).
+     */
+    private void disableCharacters()
+    {
+        Node root = _app.getRootNode();
+        PhysicsSpace space = _bulletAppState.getPhysicsSpace();
+
+        for (CharacterManagerControl character : _characters)
+        {
+            character.disableCharacter(root, space);
+        }
+    }
+    
+    /**
+     * Enables all characters currently to be simulated (attaches to root node and physics space).
+     */
+    private void enableCharacters()
+    {
+        Node root = _app.getRootNode();
+        PhysicsSpace space = _bulletAppState.getPhysicsSpace();
+
+        for (CharacterManagerControl character : _characters)
+        {
+            character.enableCharacter(root, space);
+        }
+    }
+    
+    /**
+     * Resizes simulated characters list.
+     * @param size 
+     */
+    private void setCharacterListSize(int size)
+    {
+        while (_characters.size() < size)
+        {
+            _characters.add(createCharacter());
+        }
+
+        while (_characters.size() > size)
+        {
+            _characters.remove(_characters.size() - 1);
+        }
     }
     
 }
--- blastback_client/src/com/blastback/controls/CharacterManagerControl.java
+++ blastback_client/src/com/blastback/controls/CharacterManagerControl.java
@@ -5,8 +5,14 @@
  */
 package com.blastback.controls;
 
+import com.blastback.shared.messages.data.PlayerStateInfo;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 
 /**
@@ -15,6 +21,26 @@ import com.jme3.scene.control.AbstractControl;
  */
 public class CharacterManagerControl extends AbstractControl
 {
+    private int _id;
+    private CharacterControl _charControl;
+    //TODO: INTERPOLATION DATA
+    
+    public CharacterManagerControl(int id)
+    {
+        _id = id;
+    }
+    
+    public CharacterManagerControl()
+    {
+        _id = -1;
+    }
+
+    @Override
+    public void setSpatial(Spatial spatial)
+    {
+        super.setSpatial(spatial);
+        _charControl = spatial.getControl(CharacterControl.class);
+    }
 
     @Override
     protected void controlUpdate(float tpf)
@@ -27,5 +53,47 @@ public class CharacterManagerControl extends AbstractControl
     {
         
     }
+    
+    public int getId()
+    {
+        return _id;
+    }
+
+    public void setId(int _id)
+    {
+        this._id = _id;
+    }
+    
+    public void setFromState(PlayerStateInfo state)
+    {
+        _id = state.getClientId();
+        setPosition(state.getPlayerState().getLocalTranslation());
+        setRotation(state.getPlayerState().getLocalRotation());
+    }
+    
+    public void setPosition(Vector3f position)
+    {
+        _charControl.setPhysicsLocation(position);
+    }
+    
+    public void setRotation(Vector3f rotation)
+    {
+        _charControl.setViewDirection(rotation);
+    }
+
+    public void enableCharacter(Node root, PhysicsSpace space)
+    {
+        root.detachChild(spatial);
+        space.remove(spatial);
+    }
+    
+    public void disableCharacter(Node root, PhysicsSpace space)
+    {
+        root.attachChild(spatial);
+        space.add(spatial);
+    }
+
+    
+    
     
 }
